01bf41ba20471303edd2f20b6034347142b85665
Eleftherios <lefpoge@gmail.com>
2020-04-07 20:50:35 +0300

    make more Components

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,29 @@
     <h1>ToDo App</h1>
     <div class="todo-wrapper">
       <div class="todo-container">
-        <div class="todo-list">
-          <div class="todo-item">
-            <div class="todo-item-content">
-              <div class="todo-item-title">
-                Walk
-              </div>
-              <div class="todo-item-description">
-                Go to Forest
-              </div>
-            </div>
-          </div>
-        </div>
+        <TodoList />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+
+import TodoList from "@/components/TodoList"
+
+
   export default {
     name: 'App',
     data() {
 
     },
     components: {
-
+      TodoList
     },
   }
 </script>
 
-<style lang="scss" >
+<style lang="scss" scoped >
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -42,36 +35,20 @@
     margin-top: 60px;
   }
 
-  .todo{
-    &-wrapper{
-    margin: 0 auto;
-    width: 400px;
-    min-height: 400px;
-    background-color: #ededed;
-    } 
+  .todo {
+    &-wrapper {
+      margin: 0 auto;
+      width: 400px;
+      min-height: 400px;
+      background-color: #ededed;
+    }
+
     &-container {
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    border-radius: 5px;
-  }
-  &-item{
-    background-color: #2c3e50;
-    min-height: 70px;
-    margin: 10px;
-    padding: 10px;
-    color: #fff;
-    border-radius: 10px;
-    font-size: 23px;
-  }
-
-  &-item-title{
-    font-weight: bold;
-  }
-
-  &-item-description{
-    font-size:19px;
-  }
+      display: flex;
+      justify-content: center;
+      width: 100%;
+      border-radius: 5px;
+    }
 
   }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="todo-item">
+        <div class="todo-item-content">
+            <div class="todo-item-title">
+                Walk
+            </div>
+            <div class="todo-item-description">
+                Go to Forest
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+    .todo {
+        &-item {
+            background-color: #2c3e50;
+            min-height: 70px;
+            margin: 10px;
+            padding: 10px;
+            color: #fff;
+            border-radius: 10px;
+            font-size: 23px;
+        }
+
+        &-item-title {
+            font-weight: bold;
+        }
+
+        &-item-description {
+            font-size: 19px;
+        }
+    }
+</style>
