ab9f2fa95396afdf6d909f9d9199cfdae5d6cc89
Eleftherios <lefpoge@gmail.com>
2020-04-07 20:31:21 +0300

    ui  part 1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,77 @@
 <template>
   <div id="app">
-    <h1>Hello World</h1>
+    <h1>ToDo App</h1>
+    <div class="todo-wrapper">
+      <div class="todo-container">
+        <div class="todo-list">
+          <div class="todo-item">
+            <div class="todo-item-content">
+              <div class="todo-item-title">
+                Walk
+              </div>
+              <div class="todo-item-description">
+                Go to Forest
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import playground from "./playground"
+  export default {
+    name: 'App',
+    data() {
 
-export default {
-  name: 'App',
-  components: {
-    
-  },
-  created(){
-    playground()
+    },
+    components: {
+
+    },
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" >
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .todo{
+    &-wrapper{
+    margin: 0 auto;
+    width: 400px;
+    min-height: 400px;
+    background-color: #ededed;
+    } 
+    &-container {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    border-radius: 5px;
+  }
+  &-item{
+    background-color: #2c3e50;
+    min-height: 70px;
+    margin: 10px;
+    padding: 10px;
+    color: #fff;
+    border-radius: 10px;
+    font-size: 23px;
+  }
+
+  &-item-title{
+    font-weight: bold;
+  }
+
+  &-item-description{
+    font-size:19px;
+  }
+
+  }
 </style>
--- src/playground.js
+++
@@ -1,6 +0,0 @@
-
-
-export default function(){
-    console.log("Hello wolrd!!");
-    
-}
